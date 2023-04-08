9891fdd4d0d079f822a6b74d0258c06be7da2dd1
Yaroslav <kogutyaroslav3947@gmail.com>
2023-04-08 13:03:18 +0300

    rm rubbish

--- main.cpp
+++ main.cpp
@@ -7,28 +7,22 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    // Create main window
     QMainWindow mainWindow;
 
-    // Create central widget
+
     QWidget centralWidget(&mainWindow);
 
-    // Create a button
     QPushButton button("Hello World!", &centralWidget);
     button.move(10, 10);
     button.show();
 
-    // Create a label
     QLabel label("Welcome to my Qt Application", &centralWidget);
     label.move(10, 50);
     label.show();
 
-    // Set central widget
     mainWindow.setCentralWidget(&centralWidget);
 
-    // Show main window
     mainWindow.show();
 
-    // Run application
     return a.exec();
 }
